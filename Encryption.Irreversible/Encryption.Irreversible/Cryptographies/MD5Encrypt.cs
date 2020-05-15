1d5d12e152afac59d3d380a342ddcd0864904477
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-05-15 15:09:39 +0800

    feature
    添加MD5加密

---
+++ Encryption.Irreversible/Encryption.Irreversible/Cryptographies/MD5Encrypt.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption.Irreversible
+{
+    public class MD5Encrypt
+    {
+        /// <summary>
+        /// 获取16字节大写MD5编码字符串
+        /// </summary>
+        /// <param name="buff">编码</param>
+        /// <returns></returns>
+        public static string Encrypt(byte[] buff)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] output = md5.ComputeHash(buff);
+            return BitConverter.ToString(output).Replace("-", "");
+        }
+
+        public static string Encrypt(string input)
+        {
+            var buff = Encoding.ASCII.GetBytes(input);
+            return Encrypt(buff);
+        }
+
+        /// <summary>
+        /// 获取16字节大写MD5编码字符串
+        /// </summary>
+        /// <param name="buff">编码</param>
+        /// <returns></returns>
+        public static string Encrypt1(byte[] buff)
+        {
+            MD5 md5Hasher = MD5.Create();
+            byte[] bytesMD5 = md5Hasher.ComputeHash(buff);
+            return BitConverter.ToString(bytesMD5).Replace("-", "");
+        }
+
+        public static string Encrypt1(string input)
+        {
+            var buff = Encoding.ASCII.GetBytes(input);
+            return Encrypt1(buff);
+        }
+    }
+}
--- Encryption.Irreversible/Encryption.Irreversible/Program.cs
+++ Encryption.Irreversible/Encryption.Irreversible/Program.cs
@@ -10,6 +10,11 @@ namespace Encryption.Irreversible
     {
         static void Main(string[] args)
         {
+            var md5Str = MD5Encrypt.Encrypt("user_12345");
+            Console.WriteLine(md5Str);
+
+            md5Str = MD5Encrypt.Encrypt1("user_12345");
+            Console.WriteLine(md5Str);
         }
     }
 }
