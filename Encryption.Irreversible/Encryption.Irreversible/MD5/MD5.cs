2d252e4ef43b5e21542b1a505d9c081200c649d6
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-05-15 14:11:13 +0800

    doc：创建不可逆加密项目

---
+++ Encryption.Irreversible/Encryption.Irreversible/MD5/MD5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption.Irreversible
+{
+    public class MD5
+    {
+        public static string Encrypt(string input)
+        {
+
+        }
+    }
+}
