f90d53de5ab0c34faa8180039eaf59c1947aa6f0
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-05-21 13:29:42 +0800

    feature
    添加AES CBC算法
    测试站点http://tool.chacuo.net/cryptaes，CBC-pkcs5padding

---
+++ src/Encryption.Irreversible/Encryption.Irreversible/Model/TokenModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption.Irreversible.Model
+{
+    public class TokenModel
+    {
+        public string userId { get; set; }
+        public string nonce { get; set; }
+    }
+}
--- src/Encryption.Irreversible/Encryption.Irreversible/Program.cs
+++ src/Encryption.Irreversible/Encryption.Irreversible/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Encryption.Irreversible.Cryptographies;
+using Encryption.Irreversible.Model;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +14,69 @@ namespace Encryption.Irreversible
     {
         static void Main(string[] args)
         {
-            ContentMD5();
         }
 
+        #region AES CBC
+
+        private static void GetAes()
+        {
+            var request = new TokenModel
+            {
+                userId = "12564",
+                nonce = "a9028aad1c7a611a",
+            };
+
+            var original = JsonConvert.SerializeObject(request);
+            var key = "DWYEiW9kqtRs21da";
+            var iv = Guid.NewGuid().ToString("N").Substring(0, 16);
+
+            var base64 = AesEncrypt.EncryptStringToBase64(original, key, iv);
+            Console.WriteLine(key);
+            Console.WriteLine(iv);
+            Console.WriteLine(base64);
+        }
+
+        private static void AES()
+        {
+            string original = "Here is some data to encrypt!";
+            var key = "DWYEiW9kqtRs21da";
+
+            var iv = Guid.NewGuid().ToString("N").Substring(0, 16);
+            Console.WriteLine(key);
+            Console.WriteLine(iv);   
+            var str = AesEncrypt.EncryptStringToBase64(original, key, iv);
+            Console.WriteLine(str);
+
+            var org = AesEncrypt.DecryptBase64ToOriginal(str, key, iv);
+            Console.WriteLine(org);
+            Console.WriteLine();
+        }
+
+        private static void AESCBC()
+        {
+            string original = "Here is some data to encrypt!";
+
+            using (Aes myAes = Aes.Create())
+            {
+                // Encrypt the string to an array of bytes.
+                byte[] encrypted = Cryptographies.AesEncrypt.EncryptStringToBytes_Aes(original, myAes.Key, myAes.IV);
+
+                Console.WriteLine($"Key: {BitConverter.ToString(myAes.Key)}");
+                Console.WriteLine($"Key: {Encoding.UTF8.GetString(myAes.Key)}");
+                Console.WriteLine($" IV: {BitConverter.ToString(myAes.IV)}");
+                Console.WriteLine($" IV: {Encoding.UTF8.GetString(myAes.IV)}");
+                Console.WriteLine($"Base64: {Convert.ToBase64String(encrypted)}");
+
+                // Decrypt the bytes to a string.
+                string roundtrip = Cryptographies.AesEncrypt.DecryptStringFromBytes_Aes(encrypted, myAes.Key, myAes.IV);
+
+                //Display the original data and the decrypted data.
+                Console.WriteLine("Original:   {0}", original);
+                Console.WriteLine("Round Trip: {0}", roundtrip);
+            }
+        }
+
+        #endregion
         private static void ContentMD5()
         {
             var str = "Hello World!";
