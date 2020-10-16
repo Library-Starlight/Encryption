3f5237db9a7b56df012f8c07988e4a65e44ea872
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-10-16 17:10:46 +0800

    feature
    修改MD5加密默认命名空间

--- src/Encryption.Irreversible/Cryptographies/MD5Encrypt.cs
+++ src/Encryption.Irreversible/Cryptographies/MD5Encrypt.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Encryption.Irreversible
+namespace System.Security.Cryptography
 {
     public class MD5Encrypt
     {
